f8cb6b8552af347050b088c279e9b16421985de8
Marius <grosskopf81@gmail.com>
2020-11-28 23:19:49 +0100

    update structure and add server

---
+++ server/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var cors = require('cors')
+
+
+const postRoutes = require('./api/routes/post');
+const userRoutes = require('./api/routes/user');
+
+
+mongoose.connect("mongodb://localhost:27017").then(result => {
+    console.log("connected");
+}
+
+
+);
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Alllow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
+const http = require('http');
+const app = require('./app');
 
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+console.log(port);
+
+
+server.listen(port);
